a44b12130d6211cc87f6d0edf94a12d185ac0999
Linfeng Qian <thewawar@gmail.com>
2019-03-22 16:17:21 +0800

    Fix send listen addresses bug

--- protocols/identify/src/lib.rs
+++ protocols/identify/src/lib.rs
@@ -4,7 +4,7 @@ mod protocol_generated;
 
 mod protocol;
 
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::time::{Duration, Instant};
 
 use log::{debug, error, trace};
@@ -150,13 +150,9 @@ impl<T: AddrManager> ServiceProtocol for IdentifyProtocol<T> {
         trace!("IdentifyProtocol sconnected from {:?}", remote_info.peer_id);
         self.remote_infos.insert(session.id, remote_info);
 
-        let listen_addrs: HashSet<Multiaddr> = self
-            .observed_addrs
-            .values()
-            .take(MAX_ADDRS)
-            .cloned()
-            .collect();
-        let data = IdentifyMessage::ListenAddrs(listen_addrs.into_iter().collect()).encode();
+        let listen_addrs: Vec<Multiaddr> =
+            self.listen_addrs.iter().take(MAX_ADDRS).cloned().collect();
+        let data = IdentifyMessage::ListenAddrs(listen_addrs).encode();
         service.send_message(session.id, self.id, data);
 
         let observed_addr = session
