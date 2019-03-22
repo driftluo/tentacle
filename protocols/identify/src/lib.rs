0afb699fd9e10101e809c09314a34c7fa58ee55d
Linfeng Qian <thewawar@gmail.com>
2019-03-22 15:33:50 +0800

    Tiny fix

--- protocols/identify/src/lib.rs
+++ protocols/identify/src/lib.rs
@@ -153,7 +153,6 @@ impl<T: AddrManager> ServiceProtocol for IdentifyProtocol<T> {
         let listen_addrs: HashSet<Multiaddr> = self
             .observed_addrs
             .values()
-            .chain(self.listen_addrs.iter())
             .take(MAX_ADDRS)
             .cloned()
             .collect();
@@ -234,6 +233,7 @@ impl<T: AddrManager> ServiceProtocol for IdentifyProtocol<T> {
                 } else {
                     trace!("received observed address: {}", addr);
 
+                    // TODO: this address shoulde be reachable
                     // Add transform observed address to local listen address list
                     if let Some(socket_addr) = multiaddr_to_socketaddr(&addr) {
                         let observed_ip = socket_addr.ip();
@@ -265,8 +265,8 @@ impl<T: AddrManager> ServiceProtocol for IdentifyProtocol<T> {
                                         .collect()
                                 })
                             {
-                                self.listen_addrs.push(new_listen_addr);
                                 // TODO how can we trust this address?
+                                self.listen_addrs.push(new_listen_addr);
                                 if self
                                     .addr_mgr
                                     .add_observed_addr(&info.peer_id, addr.clone())
