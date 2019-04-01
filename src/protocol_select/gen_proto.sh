2e5d56ad975558732b34e1cffa814f3ad84eaf52
ian <ian@nervos.org>
2019-04-01 10:42:52 +0800

    chore: cleanup gen_proto.sh
    Use `make gen-fb` to generate files, and `make clean-fb gen-fb` to regenerate
    all files.

--- src/protocol_select/gen_proto.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-# flatc version 1.10.0
-# pip install cfbc
-
-flatc --rust protocol_select.fbs
-flatc -b --schema protocol_select.fbs
-cfbc protocol_select.bfbs
-rm *_builder.rs protocol_select.bfbs
