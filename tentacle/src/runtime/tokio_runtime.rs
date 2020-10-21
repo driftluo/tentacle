7926cbc5c7aad0824d7dfaf0d82fa73120edbcc7
driftluo <driftluo@foxmail.com>
2020-10-21 19:27:45 +0800

    fix: fix windows compatible

--- tentacle/src/runtime/tokio_runtime.rs
+++ tentacle/src/runtime/tokio_runtime.rs
@@ -18,5 +18,17 @@ pub fn from_std(listen: StdListen) -> io::Result<TcpListener> {
 }
 
 pub async fn connect_std(stream: Socket, addr: &SocketAddr) -> io::Result<TcpStream> {
+    // on windows, if not set reuse address, but use socket2 and tokio `connect_std` function
+    // will cause a error "Os {code: 10022, kind: InvalidInput, message: "An invalid parameter was provided." }"
+    // but if set, nothing happened, this is confusing behavior
+    // issue: https://github.com/tokio-rs/tokio/issues/3030
+    #[cfg(windows)]
+    if stream.reuse_address()? {
+        TcpStream::connect_std(stream.into_tcp_stream(), addr).await
+    } else {
+        TcpStream::connect(addr).await
+    }
+
+    #[cfg(unix)]
     TcpStream::connect_std(stream.into_tcp_stream(), addr).await
 }
